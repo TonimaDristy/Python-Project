480c29372d9864dfe48f7117562d940af9deedf2
TonimaDristy <tidristy477@gmail.com>
2026-02-21 23:37:13 +0600

    Take number input

--- main.py
+++ main.py
@@ -14,3 +14,13 @@ def main():
  # Take user input
     name = input("Enter your name: ")
     greet(name)   
+
+
+  # Take number input
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    
+    result = add_numbers(num1, num2)
+    print("Sum is:", result)
+
+    print("=== Program Finished ===")   

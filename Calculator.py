c723d0cd3e3178f61d5eeb0efb37ab0251854627
TonimaDristy <tidristy477@gmail.com>
2026-02-27 21:58:00 +0600

    Leap Year Checker

--- Calculator.py
+++ Calculator.py
@@ -30,3 +30,21 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    return False
+
+def main():
+    print("=== Leap Year Checker ===")
+
+    year = int(input("Enter year: "))
+
+    if is_leap_year(year):
+        print(year, "is a leap year")
+    else:
+        print(year, "is not a leap year")
+
+if __name__ == "__main__":
+    main()

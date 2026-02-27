43f40401082866b9257379ca316a53b2236b15d7
TonimaDristy <tidristy477@gmail.com>
2026-02-27 22:01:34 +0600

    Here are 3 short and simple main.py programs:42. Number Positive / Negative

--- Calculator.py
+++ Calculator.py
@@ -138,3 +138,18 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    def check_number(num):
+    if num > 0:
+        return "Positive"
+    elif num < 0:
+        return "Negative"
+    else:
+        return "Zero"
+
+def main():
+    num = int(input("Enter a number: "))
+    print("The number is:", check_number(num))
+
+if __name__ == "__main__":
+    main()

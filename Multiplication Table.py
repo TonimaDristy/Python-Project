2d9644e257c89314be0f732a34af0fd16213d97f
TonimaDristy <tidristy477@gmail.com>
2026-03-05 22:06:10 +0600

    Number is Prime

--- Multiplication Table.py
+++ Multiplication Table.py
@@ -23,3 +23,23 @@ if __name__ == "__main__":
 
 if __name__ == "__main__":
     main()
+    def is_prime(num):
+    if num <= 1:
+        return False
+
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+
+    return True
+
+def main():
+    num = int(input("Enter number: "))
+
+    if is_prime(num):
+        print("Prime number")
+    else:
+        print("Not prime")
+
+if __name__ == "__main__":
+    main()

52aa03e3b7d7c7e079b82292b0e05dbe7d7b3def
TonimaDristy <tidristy477@gmail.com>
2026-03-05 22:08:40 +0600

    List is sorted

--- Multiplication Table.py
+++ Multiplication Table.py
@@ -76,3 +76,13 @@ if __name__ == "__main__":
 
 if __name__ == "__main__":
     main()
+    def main():
+    numbers = list(map(int, input("Enter numbers separated by space: ").split()))
+
+    if numbers == sorted(numbers):
+        print("List is sorted")
+    else:
+        print("List is not sorted")
+
+if __name__ == "__main__":
+    main()

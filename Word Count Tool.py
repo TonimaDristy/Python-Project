03d7b394abc9fc7fd22841c414a7d0438c051b77
TonimaDristy <tidristy477@gmail.com>
2026-03-19 22:24:12 +0600

    Line Count

--- Word Count Tool.py
+++ Word Count Tool.py
@@ -4,3 +4,7 @@ words = text.split()
 word_count = len(words)
 char_count = len(text)
 line_count = text.count("\n") + 1
+
+print("Word Count:", word_count)
+print("Character Count:", char_count)
+print("Line Count:", line_count)

cb1ec707f2a2d2be3788045799ad7f2371552222
TonimaDristy <tidristy477@gmail.com>
2026-03-24 21:55:04 +0600

    Color Picker

--- Dice Rolling Simulator.py
+++ Dice Rolling Simulator.py
@@ -1,15 +1,12 @@
 import random
 
-while True:
-    number = random.randint(1, 10)
-    
-    guess = int(input("Guess a number (1-10): "))
-    
-    if guess == number:
-        print("Correct!")
-    else:
-        print("Wrong! The number was:", number)
+colors = ["Red", "Blue", "Green", "Yellow", "Purple"]
 
-    again = input("Play again? (y/n): ")
+while True:
+    input("Press Enter to pick a random color...")
+    
+    print("Color:", random.choice(colors))
+
+    again = input("Pick again? (y/n): ")
     if again.lower() != "y":
         break

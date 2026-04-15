33f4503ab17ca80f15741a4a8389a38c0eb3d702
TonimaDristy <tidristy477@gmail.com>
2026-04-15 23:19:31 +0600

    Interest Calculator

--- Temperature Converter.py
+++ Temperature Converter.py
@@ -1,18 +1,8 @@
-def c_to_f(c):
-    return (c * 9/5) + 32
+def simple_interest(p, r, t):
+    return (p * r * t) / 100
 
-def f_to_c(f):
-    return (f - 32) * 5/9
+p = float(input("Principal: "))
+r = float(input("Rate: "))
+t = float(input("Time: "))
 
-print("1. C to F")
-print("2. F to C")
-
-choice = input("Choice: ")
-
-if choice == "1":
-    c = float(input("Celsius: "))
-    print("Fahrenheit:", c_to_f(c))
-
-elif choice == "2":
-    f = float(input("Fahrenheit: "))
-    print("Celsius:", f_to_c(f))
+print("Simple Interest:", simple_interest(p, r, t))
